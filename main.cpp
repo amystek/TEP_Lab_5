a3212e99184fc00fbe415d1c15d38f22bcf94ea7
amystek <arkadiusz.mystek@gmail.com>
2020-06-05 22:55:07 +0200

    Added class CTreeDynamic with included class CNodeDynamic

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include "main.h"
 
-int main() {
-    CTreeStatic c_tree;
-    c_tree.v_tree_test();   //create test tree
-    c_tree.vPrintTree();
+int main()
+{
+    vStaticTreeTest();
+    vDynamicTreeTest();
     return 0;
-}
+} //int main()
+
+void vStaticTreeTest()
+{
+    cout << endl << "vStaticTreeTest()" << endl;
+    CTreeStatic c_tree_stat;
+    c_tree_stat.v_tree_test();   //create test tree
+    c_tree_stat.vPrintTree();
+} //void vStaticTreeTest()
+
+void vDynamicTreeTest()
+{
+    cout << endl << "vDynamicTreeTest()" << endl;
+    CTreeDynamic c_tree_dyn;
+    c_tree_dyn.v_tree_test();   //create test tree
+    c_tree_dyn.vPrintTree();
+} //void vDynamicTreeTest()
 
--- main.h
+++ main.h
@@ -2,9 +2,12 @@
 #define LAB_5_MAIN_H
 
 #include "CTree/CTreeStatic.h"
+#include "CTree/CTreeDynamic.h"
 
 using namespace std;
 
 int main();
+void vStaticTreeTest();
+void vDynamicTreeTest();
 
 #endif //LAB_5_MAIN_H
